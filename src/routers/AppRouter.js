032d8ef28ff23a459c7ceb7f2444ad0e58c4d395
Germán Moreno <grmmrno@gmail.com>
2020-12-15 13:15:46 -0400

    HeroesApp terminada

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PublicRoute exact
+                        path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={user.logged}
+                    />
+
+                    <PrivateRoute
+                        path="/"
+                        component={DashboardRoutes}
+                        isAuthenticated={user.logged}
+                    />
                 </Switch>
             </div>
         </Router>
